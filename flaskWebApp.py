7466710ed03d1b9752e54f409fefe2499a45e11e
Adam Miros <adasmiros@gmail.com>
2020-03-19 17:55:45 -0400

    Added support for creating TXT files! Now when submitting forms, the TXT file is created and populated accordingly

--- flaskWebApp.py
+++ flaskWebApp.py
@@ -1,15 +1,30 @@
 from flask import Flask, redirect, url_for, render_template, request
 
+
+#Importing Method to generate TXT file
+from generateTXTFile import generateTXTFile
+
+#This is needed for flask framework to start
 app = Flask(__name__)
 
-
-@app.route("/", methods=["POST", "GET"])
+#Main view presented to user which will be used to collect information
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("home.html")
+    if request.method == "GET":
+        return render_template("home.html")
+
+    elif request.method == "POST":
+        firstName = request.form["firstName"]
+        lastName = request.form["lastName"]
+        netID = request.form["NETID"]
+        department = request.form["Department"]
 
 
 
 
+
+        generateTXTFile(firstName, lastName, netID, department)
+        return render_template("home.html")
 
 
 @app.route("/sucess")
---
+++ generateTXTFile.py
@@ -0,0 +1,10 @@
+#Method to create TXT file
+
+def generateTXTFile(firstName, lastName, netID, department):
+    f = open((firstName + "_" + lastName + ".txt"), "w+")
+    f.write("First Name: " + firstName + "\n")
+    f.write("Last Name: " + lastName + "\n")
+    f.write("NetID: " + netID + "\n")
+    f.write("Department: " + department + "\n")
+
+    f.close()
